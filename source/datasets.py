cfb5698a13443f91489e7c326863560a068b626c
Arty <artygadetsky@yandex.ru>
2018-05-25 05:12:45 +0300

    added DefinitionModelingDataset

--- source/datasets.py
+++ source/datasets.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset, DataLoader
 import constants
 import json
+import numpy as np
 
 
 class Vocabulary:
@@ -20,6 +21,33 @@ class Vocabulary:
             constants.EOS_IDX: constants.EOS
         }
 
+        # we need this for maxlen of word being definedR in CH conditioning
+        self.tok_maxlen = -float("inf")
+
+    def encode(self, tok):
+        if tok in self.tok2id:
+            return self.tok2id[tok]
+        else:
+            return constants.UNK_IDX
+
+    def decode(self, idx):
+        if idx in self.id2tok:
+            return self.id2tok[idx]
+        else:
+            raise ValueError("No such idx: {0}".format(idx))
+
+    def encode_seq(self, arr):
+        ret = []
+        for elem in arr:
+            ret.append(self.encode(elem))
+        return ret
+
+    def decode_seq(self, arr):
+        ret = []
+        for elem in arr:
+            ret.append(self.decode(elem))
+        return ret
+
     def add_token(self, tok):
         if tok not in self.tok2id:
             self.tok2id[tok] = len(self.tok2id)
@@ -27,23 +55,15 @@ class Vocabulary:
 
     def save(self, path):
         with open(path, "w") as outfile:
-            json.dump(self.id2tok, outfile, indent=4)
+            json.dump([self.id2tok, self.tok_maxlen], outfile, indent=4)
 
     def load(self, path):
         with open(path, "r") as infile:
-            self.id2tok = json.load(infile)
+            self.id2tok, self.tok_maxlen = json.load(infile)
         self.id2tok = {int(k): v for k, v in self.id2tok.items()}
         self.tok2id = {}
         for i in self.id2tok.keys():
             self.tok2id[self.id2tok[i]] = i
-
-
-def LanguageModelingCollate(batch):
-    pass
-
-
-def DefinitionModelingCollate(batch):
-    pass
 
 
 class LanguageModelingDataset(Dataset):
@@ -65,25 +85,118 @@ class LanguageModelingDataset(Dataset):
         pass
 
 
+def LanguageModelingCollate(batch):
+    pass
+
+
 class DefinitionModelingDataset(Dataset):
     """DefinitionModeling dataset."""
 
-    def __init__(self, file, vocab, input_vectors=None,
-                 input_adaptive_vectors=None, context_vocab=None,
-                 ch_vocab=None):
+    def __init__(self, file, vocab_path, input_vectors_path,
+                 input_adaptive_vectors_path, context_vocab_path,
+                 ch_vocab_path, use_seed):
         """
         Args:
             file (string): Path to the file.
-            vocab (Vocabulary): word vocab to use
-            input_vectors (ndarray): vectors for Input conditioning
-            input_adaptive_vectors (ndarray): vectors for Input-Adaptive conditioning
-            context_vocab (Vocabulary): vocab for context words for Input-Attention
-            ch_vocab (Vocabulary): char vocab for CH conditioning
+            vocab_path (string): path to word vocab to use
+            input_vectors_path (string): path to vectors for Input conditioning
+            input_adaptive_vectors_path (string): path to vectors for Input-Adaptive conditioning
+            context_vocab_path (string): path to vocab for context words for Input-Attention
+            ch_vocab_path (string): path to char vocab for CH conditioning
+            use_seed (bool): whether to use Seed conditioning or not
         """
-        pass
+        with open(file, "r") as infile:
+            self.data = json.load(infile)
+        self.voc = Vocabulary()
+        self.voc.load(vocab_path)
+        self.context_voc = Vocabulary()
+        self.context_voc.load(context_vocab_path)
+        self.ch_voc = Vocabulary()
+        self.ch_voc.load(ch_vocab_path)
+        self.input_vectors = np.load(input_vectors_path)
+        self.input_adaptive_vectors = np.load(input_adaptive_vectors_path)
+        self.use_seed = use_seed
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     def __getitem__(self, idx):
-        pass
+        sample = {
+            "x": self.voc.encode_seq(self.data[idx][1]),
+            "y": self.voc.encode_seq(self.data[idx][1][1:] + [constants.EOS]),
+            "input": self.input_vectors[idx],
+            "input_adaptive": self.input_adaptive_vectors[idx],
+            "word": self.context_voc.encode(self.data[idx][0][0]),
+            "context": self.context_voc.encode_seq(self.data[idx][2]),
+            "CH": [constants.BOS_IDX] + self.ch_voc.encode_seq(list(self.data[idx][0][0])) + [constants.EOS_IDX],
+            "CH_maxlen": self.voc.tok_maxlen + 2
+        }
+        # CH_maxlen: +2 because EOS + BOS
+        if self.use_seed:
+            sample["y"] = [sample["x"][0]] + sample["y"]
+            sample["x"] = self.voc.encode_seq(self.data[idx][0]) + sample["x"]
+        return sample
+
+
+def pad(seq, size, value):
+    if len(seq) < size:
+        seq.extend([value] * (size - len(seq)))
+    return seq
+
+
+def DefinitionModelingCollate(batch):
+    batch_x = []
+    batch_y = []
+    batch_input = []
+    batch_input_adaptive = []
+    batch_word = []
+    batch_context = []
+    batch_ch = []
+    batch_lengths = []
+    context_maxlen = -float("inf")
+    CH_maxlen = batch[0]["CH_maxlen"]
+    definition_lengths = []
+    for i in range(len(batch)):
+        batch_x.append(batch[i]["x"])
+        batch_y.append(batch[i]["y"])
+        batch_input.append(batch[i]["input"])
+        batch_input_adaptive.append(batch[i]["input_adaptive"])
+        batch_word.append(batch[i]["word"])
+        batch_context.append(batch[i]["context"])
+        batch_ch.append(batch[i]["CH"])
+        context_maxlen = max(context_maxlen, len(batch_context[-1]))
+        definition_lengths.append(len(batch_x[-1]))
+
+    definition_maxlen = max(definition_lengths)
+
+    for i in range(len(batch)):
+        batch_x[i] = pad(batch_x[i], definition_maxlen, constants.PAD_IDX)
+        batch_y[i] = pad(batch_y[i], definition_maxlen, constants.PAD_IDX)
+        batch_context[i] = pad(
+            batch_context[i], context_maxlen, constants.PAD_IDX
+        )
+        batch_ch[i] = pad(batch_ch[i], CH_maxlen, constants.PAD_IDX)
+
+    order = np.argsort(definition_lengths)[::-1]
+    batch_x = np.array(batch_x)[order]
+    batch_y = np.array(batch_y)[order]
+    batch_input = np.array(batch_input)[order]
+    batch_input_adaptive = np.array(batch_input_adaptive)[order]
+    batch_word = np.array(batch_word)[order]
+    batch_context = np.array(batch_context)[order]
+    batch_ch = np.array(batch_ch)[order]
+
+    ret_batch = {
+        "x": batch_x,
+        "y": batch_y,
+        "xy_maxlen": definition_maxlen,
+        "input": batch_input,
+        "input_adaptive": batch_input_adaptive,
+        "word": batch_word,
+        "context": batch_context,
+        "ctx_maxlen": context_maxlen,
+        "CH": batch_ch,
+        "CH_maxlen": CH_maxlen
+    }
+
+    return ret_batch
